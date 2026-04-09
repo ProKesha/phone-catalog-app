068fd700fc2f35941215de8b9a25729a498696d1
Dmytro Popov <lort20178@gmail.com>
2026-04-09 13:53:53 +0300

    feat: add phone catalog project to portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import { CartProvider, FavoritesProvider } from './modules/shared/context';
+import { AppLayout } from './modules/shared/components/AppLayout';
+import { CartPage } from './modules/CartPage';
+import { FavoritesPage } from './modules/FavoritesPage';
+import { HomePage } from './modules/HomePage';
+import { NotFoundPage } from './modules/NotFoundPage';
+import { ProductDetailsPage } from './modules/ProductDetailsPage';
+import { ProductsPage } from './modules/ProductsPage';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <CartProvider>
+    <FavoritesProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="phones"
+              element={<ProductsPage key="phones" category="phones" />}
+            />
+            <Route
+              path="tablets"
+              element={<ProductsPage key="tablets" category="tablets" />}
+            />
+            <Route
+              path="accessories"
+              element={
+                <ProductsPage key="accessories" category="accessories" />
+              }
+            />
+            <Route path="product/:productId" element={<ProductDetailsPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="favorites" element={<FavoritesPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </FavoritesProvider>
+  </CartProvider>
+);
